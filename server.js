125fbae149fb1e0df964bcfe9ed42389dc530f91
raymonddev0 <rlqj93@gmail.com>
2018-11-18 23:36:30 +0800

    adding port

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.get('/profile/:id', (req, res)=>{profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res)=>(image.handleImage(req, res, db)))
 app.post('/imageurl', (req, res)=>(image.handleApiCall(req, res)))
 
-app.listen (3333, () => {
-	console.log('app is running on port 3333');
+app.listen (process.env.PORT || 3333, () => {
+	console.log(`app is running on port ${process.env.PORT}`);
 })
 
 /*this are the endpoints that we expect
